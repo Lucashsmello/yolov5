979b5ea182204a98fad637925315da840a238021
Lucashsmello <lucashsmello@gmail.com>
2021-04-16 17:34:25 -0300

    Small optimizations

--- yolov5predictor.py
+++ yolov5predictor.py
@@ -6,11 +6,13 @@ from .utils.datasets import preprocessImage
 from .utils.general import non_max_suppression, scale_coords
 import onnx
 import onnxruntime
+import numpy as np
 
 ###Very important!:####
 torch.backends.cudnn.benchmark = True
 torch.backends.cudnn.enabled = True
 ########################
+
 
 class Yolov5Predictor:
     def __init__(self, img_shape, conf_thres, iou_thres, yolo_model, stride=32):
@@ -26,10 +28,16 @@ class Yolov5Predictor:
         img_shape: (height, width)
         """
         if(weights_path[-5:] == '.onnx'):
+            if(onnxruntime.get_device() != 'GPU'):
+                print("WARNING: Onnx not running in GPU! (onnxruntime.get_device()='%s')" % onnxruntime.get_device())
             onnx_model = onnx.load(weights_path)
             onnx.checker.check_model(onnx_model)
             ort_session = onnxruntime.InferenceSession(weights_path)
+
+            print("onnx session providers:", ort_session.get_providers())
+            # ort_session.set_providers(['CUDAExecutionProvider'])
             yolo_model = ort_session
+
             stride = 32  # FIXME
         else:
             yolo_model = torch.load(weights_path, map_location=device, pickle_module=unpickler)['model'].float().eval()
@@ -40,7 +48,8 @@ class Yolov5Predictor:
 
     def preProcessImage(self, img0, device='cuda:0'):
         img = preprocessImage(img0, self.img_shape, stride=self.stride)
-        img = torch.from_numpy(img).to(device).float().div(255)
+        img = torch.from_numpy(img).to(device).float()
+        img /= 255
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
         return img
@@ -49,18 +58,26 @@ class Yolov5Predictor:
         # import sys
         with torch.no_grad():
             img = self.preProcessImage(img0)
+
             if(isinstance(self.yolo_model, torch.nn.Module)):
                 preds = self.yolo_model(img)[0].cpu()
             else:
+                # io_binding = self.yolo_model.io_binding()
+                # X_ortvalue = onnxruntime.OrtValue.ortvalue_from_numpy(img.cpu().numpy(), 'cuda', 0)
+
+                # io_binding.bind_input(name='images', device_type=X_ortvalue.device_name(), device_id=0,
+                #                       element_type=np.float32, shape=X_ortvalue.shape(), buffer_ptr=X_ortvalue.data_ptr())
+                # io_binding.bind_output('output')
+                # self.yolo_model.run_with_iobinding(io_binding)
+                # ort_outs = io_binding.copy_outputs_to_cpu()[0]
+
                 ort_inputs = {self.yolo_model.get_inputs()[0].name: img.cpu().numpy()}
                 ort_outs = self.yolo_model.run(None, ort_inputs)[0]
                 preds = torch.from_numpy(ort_outs)
 
-            # print(preds.shape)
-            # sys.exit(1)
             preds = non_max_suppression(preds, self.conf_thres, self.iou_thres, classes=None, agnostic=None)[0]
 
             if(preds is None):
                 return torch.empty(0)
-            preds[:, :4] = scale_coords(img.shape[2:], preds[:, :4], img0.shape).round()
+            preds[:, :4] = scale_coords(img.shape[2:], preds[:, :4], img0.shape)
         return preds
